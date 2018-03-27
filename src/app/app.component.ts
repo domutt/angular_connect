89034ddbd48eefb97d3283c5058b95668a414fb3
Dominic Muttel <Dominic.Muttel@starz.com>
2018-03-27 17:52:31 -0600

    Property and Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   appName = 'Angular App';
-  name = 'Dominic';
+  name = "Dominic's";
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
+	selector: 'app-servers',
+	templateUrl: './servers.component.html',
 
-  styleUrls: ['./servers.component.sass']
+	styleUrls: ['./servers.component.sass']
 })
 export class ServersComponent implements OnInit {
+	
+	allowNewServer = false;
+	serverCreationStatus = 'No server was created';
+	
+	constructor() { 
+	
+		setTimeout(() => {
+			this.allowNewServer = true;
+		}, 2000);
+	
+	}
 
-  constructor() { }
+	ngOnInit() {
+	}
 
-  ngOnInit() {
-  }
+	onCreateServer(){
+		this.serverCreationStatus = 'Server was created!';
+	}
 
 }
